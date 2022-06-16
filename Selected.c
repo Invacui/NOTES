4eb7b3cd0b05b526e33167ee6eb906210d84497c
Invacui <89918888+Invacui@users.noreply.github.com>
2022-06-16 12:21:53 +0530

    Update Selected.c

--- Selected.c
+++ Selected.c
@@ -1,81 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct N{
+struct N {
     int data;
     struct N *link;
-};
+}*curr,*head;
 
-/*------------------------------------------------------------------------------------------------*/
-int printdata(struct N *head)
+/*---------------------------------------------------------------------------------------------*/
+struct N* printdata(struct N *head)
 {
-    if(head == NULL){
-    printf("Null");}
-    else
-    {
+    if(head == NULL) {
+        printf("Null");
+    } else {
         struct N *ptr = (struct N *)malloc(sizeof(struct N));
         ptr = head;
-        while(ptr->link != NULL)
-        {
-            printf("%d\n", ptr->data);
+        while(ptr != NULL) {
+            printf("%d", ptr->data);
+            if(ptr->link != NULL) {
+                printf(" ->");
+            }
             ptr = ptr->link;
         }
     }
 }
-/*------------------------------------------------------------------------------------------------*/
-
-struct N* head_make(struct N *head)
+/*---------------------------------------------------------------------------------------------*/
+struct N* insert_make(struct N *head,struct N* curr)
 {
-scanf("%d", &head->data);
-head->link = NULL;   
-return head;
+    curr = (struct N*)malloc(sizeof(struct N));
+    scanf("%d", &curr->data);
+    curr->link = NULL;
+
+    while(head->link != NULL) {
+        head = head->link;
+    }
+    head-> link = curr;
+    return head;
+}
+/*---------------------------------------------------------------------------------------------*/
+
+struct N* head_make()
+{
+    head = (struct N*)malloc(sizeof(struct N));
+    scanf("%d", &head->data);
+    head->link = NULL;
+    return head;
 }
 
 /*------------------------------------------------------------------------------------------------*/
 
-int insert_make(struct N *head)
+int main()
 {
-struct N *temp = (struct N*)malloc(sizeof(struct N));
-scanf("%d", &temp->data);
-temp->link = NULL;
-struct N *ptr = (struct N *)malloc(sizeof(struct N));
-ptr = head;
-while(ptr->link != NULL)
-{
-    ptr = ptr->link;
-}
-ptr-> link = temp;
-//return head;
-}
 
-/*------------------------------------------------------------------------------------------------*/
-
-int main() {
-    struct N *head = (struct N*)malloc(sizeof(struct N));
     int Choice;
     char GG;
-    while(1)
-    {
-        
+    do {
+
         printf("Enter the choice :\n1.:Enter the First Element of the LL\n2.:Insert Element Into the LL\n3.:Print the LL\n");
         scanf("%d", &Choice);
-            switch(Choice)
-            {
-                case 1 : head_make(head);
-                        struct N *head = head_make;
-                        break;
-                case 2 : if(head == NULL || Choice == 2)
-                            {printf("Fill Up The First NODE First");}
-                            else
-                            {insert_make(head);}
-                            break;
-                case 3 : printdata(head);
-                         break;
-                
-                default : printf("Invalid Input");
+        switch(Choice) {
+        case 1 :
+            if(head == NULL) {
+                head_make();
+                break;
+            } else {
+                printf("Invalid Choice Head is already present!");
+                break;
             }
+
+        case 2 :
+            if(head == NULL && Choice == 2) {
+                printf("Fill Up The First NODE First");
+            } else {
+                insert_make(head,curr);
+            }
+            break;
+        case 3 :
+            printdata(head);
+            break;
+
+        default :
+            printf("Invalid Input");
+        }
         printf("\n Do you Want to Continue (Y/N) ?");
-        scanf("%c" , &GG);
-    }
+        scanf("\n%c" , &GG);
+    } while(GG == 'y' || GG == 'Y');
 
 }
