e90c29a39a6e725917dd0b2c31fb4d3ef36f076c
Invacui <89918888+Invacui@users.noreply.github.com>
2022-09-03 14:32:23 +0530

    Update Static.cpp

--- OOPS/Static.cpp
+++ OOPS/Static.cpp
@@ -1,4 +1,5 @@
-// Online C++ compiler to run C++ program online
+===============================================================================<NOTE>===============================================================================
+<TELLS ABOUT HOW STATIC HAS ONLY ONE INSTANCE THROUGHT THE CLASS>
 #include <iostream>
 using namespace std;
 class A{
@@ -17,5 +18,25 @@ class A obj;
 for(i = 0;i<5;i++){
 obj.func();
 }
+    return 0;
+}
+
+===============================================================================<NOTE>===============================================================================
+#include <iostream>
+using namespace std;
+class A{
+    public :
+    static int A1 ;
+    static void func()  //A Static Member Can't just access any non static Variable both need to be static 
+    {
+        static int A2;
+        cout << A1 << endl;
+    }
+};
+int A::A1 = 10;             //It is necessary to Define your S var using Scope resolution method bc linker cant link withot=ut it ,its like we made an A1 variable inside a namespace called 'A'
+int main() {
+class A obj;
+//A::A1;                //we can also assign value here but it'd be good to assign it Globaly;
+obj.func();
     return 0;
 }
