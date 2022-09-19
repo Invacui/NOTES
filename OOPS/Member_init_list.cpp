99ce32c13c021624069ef37edd617d30f4c4d172
Ashutosh Kumar <89918888+Invacui@users.noreply.github.com>
2022-09-19 17:12:49 +0530

    Create Member_init_list.cpp

---
+++ OOPS/Member_init_list.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+//MIL(Member Initilizer List)
+#include <iostream>
+#include <string>
+class framework{
+    public :
+    framework(){ std::cout<<"\nDefault contructors\n"; }                        //Def Construct
+    
+    framework(const std::string& name)
+    { std::cout<<"\nParametrized constructors with parameter: "<<name<<"\n"; }  //Par Construct
+};
+
+class Entity{
+  private :
+  std::string m_name;       //declaration of str var
+  framework E2;             //initializing object of framework entity, its its a Cons it will get auto Invoked;
+  public:
+ 
+  Entity(const std::string name)            //Par Construct
+  : E2(name)                                //Member Initializer List
+  {
+     // framework  E2(name);                //Its gonna call Parameterized Con
+      m_name = name;
+      std::cout<<"Entity ==>"<<" "<<m_name<<"\n";
+
+  }
+};
+
+int main() {
+    // Write C++ code here
+    Entity A("Ashu");
+    return 0;
+}
