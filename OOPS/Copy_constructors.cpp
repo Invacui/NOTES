8a7ddbf2a8c0def27633dff8aebd6241f3eba719
Ashutosh Kumar <89918888+Invacui@users.noreply.github.com>
2022-11-25 14:46:44 +0530

    Update Copy_constructors.cpp

--- OOPS/Copy_constructors.cpp
+++ OOPS/Copy_constructors.cpp
@@ -29,3 +29,36 @@ int main() {
     
     return 0;
 }
+/*=====================================================================================================================================================*/
+#include <iostream>
+class Entity {
+    private :
+    int x ,
+        z ,
+        y;
+    public :
+    Entity(int x, int y, int z){
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    Entity(Entity& e){
+        x = e->x;
+        y = e->y;
+        z = e->z;
+    }
+    void print(){
+        std::cout<<x<<","<<y<<","<<z<<std::endl;
+    }
+    
+};
+int main() {
+    Entity *e = new Entity(5,46,2);
+   
+    Entity f(e);
+    e.print();
+    
+    
+    return 0;
+}
+Footer
