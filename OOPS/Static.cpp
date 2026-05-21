cc4f2315792996c766c9a1214fd1ce65f73afa02
Ashutosh Kumar <Ask.72544@gmail.com>
2026-05-21 20:55:27 +0530

    Update Static.cpp
    namespace

--- OOPS/Static.cpp
+++ OOPS/Static.cpp
@@ -26,14 +26,20 @@ obj.func();
 using namespace std;
 class A{
     public :
-    static int A1 ;
+    static int A1 ; //This does not create memory It only says “Every Entity shares one integer called A1.”
     static void func()  //A Static Member Can't just access any non static Variable both need to be static 
     {
         static int A2;
         cout << A1 << endl;
     }
 };
-int A::A1 = 10;             //It is necessary to Define your S var using Scope resolution method bc linker cant link withot=ut it ,its like we made an A1 variable inside a namespace called 'A'
+//It is necessary to Define your S var using Scope resolution method bc linker cant link withot=ut it ,its like we made an A1 variable inside a namespace called 'A'
+/*
+*@eg namespace apple{void print(){cout<<"as"<<endl;}}main(){apple::print(); or namespace a = apple; a::print(); or using namespace apple::.. or just apple this will import all child from that namespace
+*@Ref Cherno Namespace and static
+*/
+int A::A1 = 10; //This Creates Memory its Like int A1 = rvalue ; [::] Is like Go inside this thing and get that member.”
+          
 int main() {
 class A obj;
 //A::A1;                //we can also assign value here but it'd be good to assign it Globaly;
